6a5671d167624d8ed91c848d8d4f726ca17722ab
ronald33 <ronald.dev1@gmail.com>
2023-01-14 09:38:47 -0500

    Agregando mas reportes internos

--- www/inkari.unsa.edu.pe/public_html/wp-content/themes/tema_inkari/page-recursos.php
+++ www/inkari.unsa.edu.pe/public_html/wp-content/themes/tema_inkari/page-recursos.php
@@ -19,157 +19,178 @@ get_template_part('page-header');
     <!-- Grid row -->
     <div class="row">
 
-    <!-- Grid column -->
-    <div class="col-lg-5 text-center">
-        <img class="img-fluid" src="<?=get_template_directory_uri();?>/images/rack.png" alt="Sample image" />
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-lg-7">
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
         <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
+        <div class="col-lg-5 text-center">
+            <img class="img-fluid" src="<?=get_template_directory_uri();?>/images/rack.png" alt="Sample image" />
         </div>
         <!-- Grid column -->
 
         <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Características de software y Hardware</h5>
-            <p class="grey-text">
-                <a href="https://drive.google.com/file/d/1yRzXWIJd4IEwXms42aML7FSTv6sevvKb/view?usp=sharing" target="_blank">Click para acceder</a>
-            </p>
-        </div>
-        <!-- Grid column -->
+        <div class="col-lg-7">
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Características de software y Hardware</h5>
+                    <p class="grey-text">
+                        <a href="https://drive.google.com/file/d/1yRzXWIJd4IEwXms42aML7FSTv6sevvKb/view?usp=sharing" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Comandos básicos de Linux</h5>
+                    <p class="grey-text">
+                        <a href="https://drive.google.com/file/d/1GNoLqch8b9hVeWc5ujzDejW8h4uZx0-k/view?usp=sharing" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Uso de Inkari</h5>
+                    <p class="grey-text">
+                        <a href="https://drive.google.com/file/d/1mEs5Xxm3hcVudRQZc_8zy9UU5JEf-vB2/view?usp=sharing" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Charla Julio 2021</h5>
+                    <p class="grey-text">
+                        <a href="https://drive.google.com/drive/folders/1oZPXgJD8o7NyPoYWLbonuoVHKHruqBFj?usp=sharing" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Segunda charla 2022</h5>
+                    <p class="grey-text">
+                        <a href="https://drive.google.com/drive/folders/1piY2CA2j2CSE5Kp8kBO6m-VDwRW79n2-?usp=share_link" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Migración de PBS a SLURM</h5>
+                    <p class="grey-text">
+                        <a href="<?=get_page_link(35);?>" target="_blank">Click para acceder</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+            <!-- Grid row -->
+            <div class="row mb-3">
+
+                <!-- Grid column -->
+                <div class="col-1">
+                    <i class="fas fa-star-of-life mt-1"></i>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-xl-10 col-md-11 col-10">
+                    <h5 class="font-weight-bold mb-3">Archivo</h5>
+                    <p class="grey-text">
+                        <ul>
+                            <li>
+                                <a href="<?=get_page_link(42);?>" target="_blank">Curso de orientación</a>
+                            </li>
+                            <li>
+                                <a href="<?=get_page_link(44);?>" target="_blank">Curso de introducción al ambiente Linux</a>
+                            </li>
+                            <li>
+                                <a href="<?=get_page_link(20);?>" target="_blank">Proyectos</a>
+                            </li>
+                            <li>
+                                <a href="<?=get_page_link(30);?>" target="_blank">Informes</a>
+                            </li>
+                        </ul>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
 
         </div>
-        <!-- Grid row -->
-
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
-        <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Comandos básicos de Linux</h5>
-            <p class="grey-text">
-                <a href="https://drive.google.com/file/d/1GNoLqch8b9hVeWc5ujzDejW8h4uZx0-k/view?usp=sharing" target="_blank">Click para acceder</a>
-            </p>
-        </div>
-        <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
-        <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Uso básico de Inkari</h5>
-            <p class="grey-text">
-                <a href="https://drive.google.com/file/d/1SvTR5xqY2uGLoZ1kfJyLjvnTxJ28N8Z6/view?usp=sharing" target="_blank">Click para acceder</a>
-            </p>
-        </div>
-        <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
-        <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Charla Julio 2021</h5>
-            <p class="grey-text">
-                <a href="https://drive.google.com/drive/folders/1oZPXgJD8o7NyPoYWLbonuoVHKHruqBFj?usp=sharing" target="_blank">Click para acceder</a>
-            </p>
-        </div>
-        <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
-        <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Migración de PBS a SLURM</h5>
-            <p class="grey-text">
-                <a href="<?=get_page_link(35);?>" target="_blank">Click para acceder</a>
-            </p>
-        </div>
-        <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-        <!-- Grid row -->
-        <div class="row mb-3">
-
-        <!-- Grid column -->
-        <div class="col-1">
-            <i class="fas fa-star-of-life mt-1"></i>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-xl-10 col-md-11 col-10">
-            <h5 class="font-weight-bold mb-3">Archivo</h5>
-            <p class="grey-text">
-                <ul>
-                    <li>
-                        <a href="<?=get_page_link(42);?>" target="_blank">Curso de orientación</a>
-                    </li>
-                    <li>
-                        <a href="<?=get_page_link(44);?>" target="_blank">Curso de introducción al ambiente Linux</a>
-                    </li>
-                    <li>
-                        <a href="<?=get_page_link(20);?>" target="_blank">Proyectos</a>
-                    </li>
-                    <li>
-                        <a href="<?=get_page_link(30);?>" target="_blank">Informes</a>
-                    </li>
-                </ul>
-            </p>
-        </div>
-        <!-- Grid column -->
-
-        </div>
-        <!-- Grid row -->
-
-    </div>
-    <!--Grid column-->
+        <!--Grid column-->
 
     </div>
     <!-- Grid row -->
--- www/inkari.unsa.edu.pe/public_html/wp-content/themes/tema_inkari/page-reportes-internos.php
+++ www/inkari.unsa.edu.pe/public_html/wp-content/themes/tema_inkari/page-reportes-internos.php
@@ -15,8 +15,37 @@ get_template_part('page-header');
 
     <ul class="text-white">
         <li>
-            Uso actual de la supercomputadora (del segmento de cálculo): <a class="btn btn-info btn-sm" target="_blank" href="http://190.119.178.178/ganglia/?c=Calculo&m=load_one&r=hour&s=by%20name&hc=4&mc=2">Ver</a>
+            Uso actual de la supercomputadora (del segmento de cálculo): <a class="btn btn-info btn-sm" target="_blank" href="http://inkari.unsa.edu.pe/ganglia/?c=Calculo&m=load_one&r=hour&s=by%20name&hc=4&mc=2">Ver</a>
         </li>
+        <li>
+            Esquema interno de Inkari: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1mJe2ISImhthHNUjyBYrvbxWqvoqdwxHZ5EltV8vVuhM/edit?usp=share_link">Ver</a>
+        </li>
+
+        <hr/>
+
+        <li>
+            Documentos de la administración (de Wilder): <a class="btn btn-info btn-sm" target="_blank" href="https://drive.google.com/drive/folders/11R7Ri4eCt-MQTH3ZWdBzGhqP8c4EhlRX?usp=share_link">Ver</a>
+        </li>
+        <li>
+            Consideraciones en la instalación de librerías: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1yO0cLtm9RRCrLq28KpOWoEnd3TIIpQ-GmEIMcwFDNLs/edit?usp=share_link">Ver</a>
+        </li>
+        <li>
+            Uso de CPU en los años 2022 y 2023: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1XlUxLFkPui4zWZJkEbyW399Oe7WWBLmj8jqnO_Dc7lY/edit?usp=sharing">Ver</a>
+        </li>
+        <li>
+            Reporte de incidencias de la supercomputadora Inkari: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1_zr_xvPHk70K4QbbMa8yLjndUDyrmjtrYPB_iQfRokQ/edit?usp=sharing">Ver</a>
+        </li>
+
+        <li>
+            Reportes automáticos: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1XeF6HFCBAurFvDGGnAq7kFKZDwZeOkhavbL1uLNXkuU/edit?usp=sharing">Ver</a>
+        </li>
+
+        <li>
+            Algunos conceptos del procesamiento distribuído: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/16oMLlBHmB6euopwCLivgdvJcHfQ7cypmcYOPLrlNdc8/edit?usp=sharing">Ver</a>
+        </li>
+
+        <hr/>
+        
         <li>
             Reporte diario de usuarios que acceden a la supercomputadora: <a class="btn btn-info btn-sm" target="_blank" href="https://drive.google.com/drive/folders/1hH6vj5hE4BWPm_N_Pz9Z1oJx1S0j8pst?usp=sharing">Ver</a>
         </li>
@@ -25,6 +54,22 @@ get_template_part('page-header');
         </li>
         <li>
             Reporte semanal de la cantidad de horas de procesador utilizadas por cada usuario: <a class="btn btn-info btn-sm" target="_blank" href="https://drive.google.com/drive/folders/1zr8luaj5Pe_9pR-fuUxdCe1CbAN1hrnE?usp=sharing">Ver</a>
+        </li>
+
+        <hr/>
+
+        <li>
+            Proceso de encendido de la supercomputadora: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1xvMhOGfXNFXL3UKRTXmCCiILTzgMWa0hInH80Q8uYLo/edit?usp=sharing">Ver</a>
+        </li>
+
+        <hr/>
+        
+        <li>
+            Informe portal para el proyecto URQU: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/document/d/1BjqHabjYoV6W2WeA5nmfsT88JrXA19oVvks4YYjtwXw/edit?usp=sharing">Ver</a>
+        </li>
+
+        <li>
+            Solicitudes de registro: <a class="btn btn-info btn-sm" target="_blank" href="https://docs.google.com/spreadsheets/d/1cWBTPwLY4oIFk0UYx9N7yBjJLkEynl7dRUM07qYXz5I/edit#gid=629575200">Ver</a>
         </li>
     </ul>
 
